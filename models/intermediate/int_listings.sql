820e71375a20624d28c5a84aea57cf41137ac2fe
Diego Diez Garcia <diegodzgarcia@gmail.com>
2026-05-23 15:40:36 +0000

    feat: adiciona mart_listings_full, testes de qualidade e filtro de preço nulo

--- models/intermediate/int_listings.sql
+++ models/intermediate/int_listings.sql
@@ -1,42 +1,30 @@
 {{ config(materialized='view') }}
-
 with listings as (
-
     select * from {{ ref('stg_listings') }}
-
 )
-
 select
     id,
     name,
     neighbourhood,
+    latitude,        
+    longitude,       
     room_type,
     property_type,
-
     accommodates,
     price,
     minimum_nights,
-
-    -- receita mínima potencial
     price * minimum_nights as min_revenue_potential,
-
-    -- classificação de preço
     case 
         when price < 100 then 'low'
         when price between 100 and 300 then 'medium'
         else 'high'
     end as price_category,
-
-    -- classificação de capacidade
     case
         when accommodates <= 2 then 'small'
         when accommodates <= 4 then 'medium'
         else 'large'
     end as size_category,
-
     review_scores_rating,
     number_of_reviews,
-
     host_is_superhost
-
 from listings
--- models/staging/stg_listings.sql
+++ models/staging/stg_listings.sql
@@ -1,35 +1,28 @@
 {{ config(materialized='view') }}
-
 with source as (
-
     select * from {{ source('airbnb_raw', 'listings') }}
-
+),
+cleaned as (
+    select
+        id,
+        name,
+        neighbourhood_cleansed as neighbourhood,
+        latitude,
+        longitude,
+        property_type,
+        room_type,
+        accommodates,
+        cast(price as float64) as price,
+        minimum_nights,
+        maximum_nights,
+        availability_30,
+        availability_365,
+        number_of_reviews,
+        review_scores_rating,
+        host_id,
+        host_is_superhost
+    from source
 )
-
-select
-    id,
-    name,
-
-    neighbourhood_cleansed as neighbourhood,
-    latitude,
-    longitude,
-
-    property_type,
-    room_type,
-    accommodates,
-
-    cast(price as float64) as price,
-
-    minimum_nights,
-    maximum_nights,
-
-    availability_30,
-    availability_365,
-
-    number_of_reviews,
-    review_scores_rating,
-
-    host_id,
-    host_is_superhost
-
-from source
+select * from cleaned
+where price is not null
+  and price > 0
